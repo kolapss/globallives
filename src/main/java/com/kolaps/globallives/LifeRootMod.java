039e9d752dea79412ef97d3b292fa532259e26f0
alexey <alekceilaptev@gmail.com>
2024-11-25 00:20:51 +0300

    fixing extralives

--- src/main/java/com/kolaps/globallives/LifeRootMod.java
+++ src/main/java/com/kolaps/globallives/LifeRootMod.java
@@ -3,31 +3,27 @@ package com.kolaps.globallives;
 
 import net.minecraft.advancements.Advancement;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.item.Item;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.command.Commands;
-import net.minecraft.command.CommandSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.scoreboard.*;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.AdvancementEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(LifeRootMod.MOD_ID)
 public class LifeRootMod {
     public static final String MOD_ID = "globallives"; // ID мода
     public static final String LIVES_TAG = "GlobalLives"; // Тег для хранения данных о жизнях
-    private static final int INITIAL_LIVES = 5; // Количество жизней по умолчанию
+    private static final int INITIAL_LIVES = 15; // Количество жизней по умолчанию
     public static final Item EtherealPage = new EtherealPageItem(new Item.Properties().tab(ItemGroup.TAB_MISC));
     public static final Item EternalScroll = new EternalScrollItem(new Item.Properties().tab(ItemGroup.TAB_MISC));
 
@@ -68,45 +64,43 @@ public class LifeRootMod {
             return score.getScore();
         }
 
-        return INITIAL_LIVES; // Возвращаем начальное значение, если Objective отсутствует
+        return -1; // Возвращаем начальное значение, если Objective отсутствует
     }
 
     @SubscribeEvent
     public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
         PlayerEntity player = event.getPlayer();
+        ServerPlayerEntity serverPlayer = (ServerPlayerEntity)player;
+        MinecraftServer server = serverPlayer.server;
 
         if (!player.level.isClientSide) {
-            CompoundNBT data = player.getPersistentData();
-            CompoundNBT persistentData = data.getCompound(PlayerEntity.PERSISTED_NBT_TAG);
 
             // Если данных о жизнях нет, задаём значение по умолчанию
-            int lives = persistentData.contains(LIVES_TAG) ? persistentData.getInt(LIVES_TAG) : INITIAL_LIVES;
-            persistentData.putInt(LIVES_TAG, lives);
-
-            // Обновляем Scoreboard
-            updatePlayerLives(player, lives);
-            player.sendMessage(new StringTextComponent("You have enter to onplayerJoin"), player.getUUID());
-            // Сохранение данных обратно
-            data.put(PlayerEntity.PERSISTED_NBT_TAG, persistentData);
+            //int lives = getPlayerLivesFromScoreboard(player)==-1 ? ServerEvents.executeCommandOnServer(server,"scoreboard objectives setdisplay list ad.Lives") : INITIAL_LIVES;
+            if(getPlayerLivesFromScoreboard(player)==-1)
+            {
+                updatePlayerLives(player, INITIAL_LIVES);
+                ServerEvents.executeCommandOnServer(server,"scoreboard objectives setdisplay list "+LIVES_TAG);
+            }
         }
     }
 
     @SubscribeEvent
-    public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
-        PlayerEntity player = event.getPlayer();
+    public void onPlayerDeath(LivingDeathEvent event) {
+        PlayerEntity player = (PlayerEntity)event.getEntity();
+        ServerPlayerEntity serverPlayer = (ServerPlayerEntity)player;
+        MinecraftServer server = serverPlayer.server;
 
         if (!player.level.isClientSide) { // Работаем только на стороне сервера
-            CompoundNBT data = player.getPersistentData();
-            CompoundNBT persistentData = data.getCompound(PlayerEntity.PERSISTED_NBT_TAG);
-
-            int lives = persistentData.contains(LIVES_TAG) ? persistentData.getInt(LIVES_TAG) : INITIAL_LIVES - 1;
-            lives = Math.max(0, lives - 1);
-            persistentData.putInt(LIVES_TAG, lives);
-
-            // Обновляем Scoreboard
-            updatePlayerLives(player, lives);
-
-            data.put(PlayerEntity.PERSISTED_NBT_TAG, persistentData); // Сохраняем данные обратно
+            int lives = getPlayerLivesFromScoreboard(player);
+            if(lives>1)
+            {
+                updatePlayerLives(player, lives-1);
+            }
+            else
+            {
+                ServerEvents.executeCommandOnServer(server,"gamemode spectator "+serverPlayer.getName().getString());
+            }
         }
     }
 
